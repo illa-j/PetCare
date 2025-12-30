d40e8ebf782ae97722570bfcfb694c6384641bff
illa-j <iliacdmn@gmail.com>
2025-12-30 16:57:53 +0200

    feat: add new entities(User, Species, Pet) and set AUTH_USER_MODEL

--- PetCare/settings.py
+++ PetCare/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 
 from pathlib import Path
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -99,6 +100,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+AUTH_USER_MODEL = "core.User"
 
 # Internationalization
 # https://docs.djangoproject.com/en/6.0/topics/i18n/
--- core/models.py
+++ core/models.py
@@ -1,3 +1,34 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.core.validators import MinValueValidator
 
-# Create your models here.
+class User(AbstractUser):
+    pass
+
+class Species(models.Model):
+    name = models.CharField(max_length=200)
+
+class Pet(models.Model):
+    owners = models.ManyToManyField(
+        User,
+        related_name="pets"
+    )
+    name = models.CharField(max_length=150, unique=True)
+    species = models.ForeignKey(
+        Species,
+        on_delete=models.CASCADE,
+        related_name="pets"
+    )
+    breed = models.CharField(max_length=200)
+    weight = models.DecimalField(
+        max_digits=5,
+        decimal_places=2,
+        validators=[MinValueValidator(0)]
+    )
+    height = models.DecimalField(
+        max_digits=5,
+        decimal_places=2,
+        validators=[MinValueValidator(0)]
+    )
+    birth_date = models.DateField()
+
