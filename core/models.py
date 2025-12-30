9baa5754d8d5af312cc5e67f116ff47768e2f4bf
illa-j <iliacdmn@gmail.com>
2025-12-30 17:49:20 +0200

    feat: add new entities (Activity, HealthEvent) and reference tables (Status, Priority)

--- core/models.py
+++ core/models.py
@@ -1,12 +1,15 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
-from django.core.validators import MinValueValidator
+from django.core.validators import MinValueValidator, MaxLengthValidator
+
 
 class User(AbstractUser):
     pass
 
+
 class Species(models.Model):
     name = models.CharField(max_length=200)
+
 
 class Pet(models.Model):
     owners = models.ManyToManyField(
@@ -32,3 +35,67 @@ class Pet(models.Model):
     )
     birth_date = models.DateField()
 
+
+class Status(models.Model):
+    name = models.CharField(max_length=150, unique=True)
+
+
+class Activity(models.Model):
+    title = models.CharField(max_length=150, unique=True)
+    description = models.TextField(
+        blank=True,
+        validators=[MaxLengthValidator(500)]
+    )
+    scheduled_date = models.DateField()
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name="activities"
+    )
+    pet = models.ForeignKey(
+        Pet,
+        on_delete=models.CASCADE,
+        related_name="activities"
+    )
+    status = models.ForeignKey(
+        Status,
+        on_delete=models.PROTECT,
+        related_name="activities"
+    )
+
+    class Meta:
+        ordering = ["scheduled_date"]
+
+class Priority(models.Model):
+    name = models.CharField(max_length=150, unique=True)
+
+class HealthEvent(models.Model):
+    title = models.CharField(max_length=150, unique=True)
+    description = models.TextField(
+        blank=True,
+        validators=[MaxLengthValidator(500)]
+    )
+    scheduled_date = models.DateField()
+    priority = models.ForeignKey(
+        Priority,
+        on_delete=models.PROTECT,
+        related_name="health_events"
+    )
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name="health_events"
+    )
+    pet = models.ForeignKey(
+        Pet,
+        on_delete=models.CASCADE,
+        related_name="health_events"
+    )
+    status = models.ForeignKey(
+        Status,
+        on_delete=models.PROTECT,
+        related_name="health_events"
+    )
+
+    class Meta:
+        ordering = ["scheduled_date"]
