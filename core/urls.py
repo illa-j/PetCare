3ca09ed88f5e5befa3e8b8017147c4ed714ce0dc
illa-j <iliacdmn@gmail.com>
2025-12-31 19:50:13 +0200

    feat: add urls for Pet, Activity, and HealthEvent list views

--- core/urls.py
+++ core/urls.py
@@ -4,13 +4,18 @@ from .views import (
     index,
     UserListView,
     UserDetailView,
+    PetListView,
+    ActivityListView,
+    HealthEventListView,
 )
-
 
 urlpatterns = [
     path("", index, name="index"),
     path("users/", UserListView.as_view(), name="users"),
     path("users/<int:pk>/", UserDetailView.as_view(), name="user_detail"),
+    path("pets/", PetListView.as_view(), name="pets"),
+    path("activities/", ActivityListView.as_view(), name="activities"),
+    path("healthevents/", HealthEventListView.as_view(), name="healthevents"),
 ]
 
 app_name = "core"
